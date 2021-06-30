eee28e97d8bcce8818a106d4f40a3217bd9591dd
Joaby <joabyoliveira2004@gmail.com>
2021-06-30 16:15:05 -0300

    feat: set up express router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const Router = require('./routes')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.use(Router)
 
 app.get('/', (req, res) => {
   res.json({
