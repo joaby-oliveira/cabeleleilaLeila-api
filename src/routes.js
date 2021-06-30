a65b9799b474feb1a0e49156d50dd22d3e45cd02
Joaby <joabyoliveira2004@gmail.com>
2021-06-30 16:13:44 -0300

    feat: create routes

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router()
+
+// Controllers
+const Client = require('./controllers/ClientController')
+
+// router.get('/client', Client.getAll)
+router.post('/client', Client.create)
+// router.delete('/client/:id', login, Client.delete)
+// router.post('/client/auth', Client.login)
+
+module.exports = router
